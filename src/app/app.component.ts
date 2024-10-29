1f31ee470ba7b56759bcb91f29861b40f4df0cc0
Cesar Salazar Rojas <20169@contraloria.gob.pe>
2024-10-29 12:15:03 -0500

    Iniciando proyecto de Angular ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VideojuegosComponent } from './videojuegos/videojuegos.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VideojuegosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'aprendiendo-angular';
+  public title = 'Cesar esta aqui yeah';
 }
